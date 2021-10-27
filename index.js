8801754de363c12e75d17f690c6f13bfb3f38a32
Gustavo Alexandre Nobre Mesquita <65964697+gustavonobreza@users.noreply.github.com>
2021-10-27 09:31:13 -0300

    chore: add warning if console not has colors

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import tty from 'node:tty';
 // TODO: Use a better method when it's added to Node.js (https://github.com/nodejs/node/pull/40240)
 const hasColors = tty.WriteStream.prototype.hasColors();
 
+if(!hasColors) console.log("[yoctocolors]: in this console, colors is not acceptable!")
+
 // Intentionally not using template literal for performance.
 const format = (startCode, endCode) => hasColors ? string => '\u001B[' + startCode + 'm' + string + '\u001B[' + endCode + 'm' : string => string;
 
